8c5b777b3f060345c02022bd8fa941bec2355459
Tuukka Koistinen <tvkoisti@gmail.com>
2021-03-16 17:33:43 +0200

    12: minigrep testing

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -9,9 +9,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("searching for: {}", config.query);
-    println!("in file: {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
